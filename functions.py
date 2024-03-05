866ebe10606a6468cf7f474416917a103a109943
brandonaccessmemory <116753343+brandonaccessmemory@users.noreply.github.com>
2024-03-05 18:31:56 +0000

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,45 @@
+import re 
+import nltk 
+from nltk.corpus import stopwords 
+
+def clean_text(text):
+    '''Clean text by removing unnecessary characters and altering the format of words.'''
+    text = text.lower()
+    
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"it's", "it is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "that is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"how's", "how is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "cannot", text)
+    text = re.sub(r"n't", " not", text)
+    text = re.sub(r"n'", "ng", text)
+    text = re.sub(r"'bout", "about", text)
+    text = re.sub(r"'til", "until", text)
+    text = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,]", "", text)
+    
+    return text
+
+def remove_stopwords(text):
+    # nltk.download('stopwords')
+    stop_words = set(stopwords.words('english'))
+   
+    filtered_words = [word for word in text.split() if word.lower() not in stop_words]
+
+    filtered_text = ' '.join(filtered_words)
+
+    return filtered_text
+
+def remove_extra_spaces(text):
+    cleaned_string = re.sub(r'\s+', ' ', text).strip()
+
+    return cleaned_string
